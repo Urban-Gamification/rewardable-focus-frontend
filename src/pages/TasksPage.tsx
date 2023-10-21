a107d0437a8c8fb9c8039f2f2c1e57d12bd32976
ahliddin <ahliddin.gaffar@gmail.com>
2023-10-21 17:41:44 +0200

    Fetching the list of goals and showing it in main page

--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -2,8 +2,66 @@ import { AppBar, Avatar, Fab, Grid, Toolbar, Typography } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import { Link } from 'react-router-dom';
 import { BottomNav, TaskCard } from '../components';
+import { config } from '../config';
+import { useAuth0 } from '@auth0/auth0-react';
+import React from 'react';
+import { set } from 'react-hook-form';
+
+
+interface Goal {
+  name: string;
+  userId: string;
+  frequency: {
+    type:
+    | "hourly"
+    | "every_2_hours"
+    | "every_4_hours"
+    | "daily"
+    | "every_2_days"
+    | "weekly"
+    | "monthly";
+  };
+  isFavourite: boolean;
+  rewardDate: Date;
+  stepGoal: string;
+  progress: number;
+  rewardEnergy: number;
+  rewardGrowth: number;
+  stepValues: string[];
+  isActive: boolean;
+}
+
+const countDaysFromTodayTillRewardDay = (rewardDate: Date) => {
+  const today = new Date();
+  const rewardDay = new Date(rewardDate);
+  const diffTime = Math.abs(rewardDay.getTime() - today.getTime());
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  return diffDays;
+};
 
 export function TasksPage() {
+  const { user, isAuthenticated } = useAuth0();
+  const [goals, setGoals] = React.useState<Goal[]>([]);
+
+  React.useEffect(() => {
+    if (isAuthenticated && user) {
+      
+      const fetchGoals = async () => {
+        // const response = await fetch(`${config.apiUrl}/goals/${user.email}`);
+        const response = await fetch(`${config.apiUrl}/goals/pavelfantastico@gmail.com`);
+        const goals = await response.json();
+        console.log(JSON.stringify(goals));
+
+        setGoals(goals);
+      }
+      fetchGoals();
+
+    }
+  }, [isAuthenticated, user]);
+
+
+
+
   return (
     <>
       <AppBar position="sticky">
@@ -23,16 +81,19 @@ export function TasksPage() {
             <Grid item>
               <Typography variant="h5">Main tasks</Typography>
             </Grid>
-            <Grid item>
-              <Grid container spacing={1}>
-                <Grid item xs={12}>
-                  <TaskCard
-                    title="Sleep 7 hours / daily"
-                    summaryText="5 ⚡ 5 🌱 Reward in: 9 days"
-                  />
-                </Grid>
-              </Grid>
-            </Grid>
+             {goals.map((goal) => (
+               <Grid item>
+                 <Grid container spacing={1}>
+                   <Grid item xs={12}>
+                     <TaskCard
+                       title={goal.name}
+                       summaryText={`${goal.rewardEnergy} ⚡ ${goal.rewardGrowth} 🌱 Reward in: ${countDaysFromTodayTillRewardDay(goal.rewardDate)} days`}
+                     />
+                   </Grid>
+                 </Grid>
+               </Grid>
+             ))}
+          
           </Grid>
         </Grid>
         <Grid item>
